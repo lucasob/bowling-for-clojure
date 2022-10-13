4226492ea9adb33ba6e71bdb53135fb37109a0a3
Lucas Bruck <lukabruck@gmail.com>
2022-10-13 22:28:25 +1100

    SCORING | Create helpers for partitioning the rolls into frames

--- src/bowling/scoring.clj
+++ src/bowling/scoring.clj
@@ -33,31 +33,40 @@
       (spare-score next-rolls)
       (reduce + frame))))
 
-(defn final-frame [rolls]
+(defn tenth-frame [rolls]
   "The score for the final frame is the same, with specific bonuses.
     * A spare: one additional roll
     * A strike: two additional rolls"
-
   (if (strike? (take 2 rolls))
     (+ 10 (apply + (subvec rolls 2)))
     (if (spare? (take 2 rolls))
       (+ (apply + (take 2 rolls)) (get rolls 2))
       (apply + (take 2 rolls)))))
 
+; The main 20 rolls of a game of bowling
+(defn main-game [all-rolls] (subvec all-rolls 0 20))
+
+; From a vector of 20 rolls, partition into frames of size 2
+(defn as-frames [all-rolls] (partition 2 all-rolls))
+
+; The first nine frames can be scored uniformly
+(defn first-nine-frames [frames] (vec (take 9 frames)))
+
+; The bonus rolls are from (20, 22]
+(defn bonus-rolls [all-rolls] (subvec all-rolls 20))
+
 (defn game-score [rolls]
   "Given a list of rolls, where the length is between 20 and 23. return the score of the game."
   ;TODO this is an eyesore and needs to be broken down accordingly
   (let
     ; the maine game frames are the first 20 rolls
-    [game-frames (vec (partition-all 2 (subvec rolls 0 20)))]
-    (let
-      ; handle the front 9 frames uniformly
-      [front-nine (vec (take 9 game-frames))]
-      (+
-        (reduce + (vec
-                    (map-indexed
-                      (fn [index frame]
-                        (frame-score frame (subvec rolls (* 2 (+ 1 index))))) front-nine)))
+    [game-frames (vec (as-frames (main-game rolls)))]
+    (+
+      (reduce
+        + (vec
+            (map-indexed
+              (fn [index frame] (frame-score frame (subvec rolls (* 2 (+ 1 index)))))
+              (first-nine-frames game-frames))))
 
-        ; The final piece is the score of frame 10 + bonus rolls
-        (final-frame (vec (concat (last game-frames) (subvec rolls 20))))))))
+      ; The final piece is the score of frame 10 + bonus rolls
+      (tenth-frame (vec (concat (last game-frames) (bonus-rolls rolls)))))))
